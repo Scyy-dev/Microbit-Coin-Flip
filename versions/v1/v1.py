f4aa8549dbd81fa50107c7dc92534cca0d620698
Scyphers <scyphers.dev@gmail.com>
2023-07-02 18:22:56 +0800

    add v1 and v2

---
+++ versions/v1/v1.py
@@ -0,0 +1,11 @@
+from microbit import *
+import random
+
+choices = [Image.SKULL, Image.HEART]
+
+while True:
+	
+	if button_a.was_pressed():
+		
+		image = random.choice(choices)
+        display.show(image)
