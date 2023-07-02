69d52415ccbc35766744086cdd15b5cc39deedb2
Scyphers <scyphers.dev@gmail.com>
2023-07-02 22:59:45 +0800

    correct mix of tabs and spaces for v2

--- versions/v2/v2.py
+++ versions/v2/v2.py
@@ -4,8 +4,7 @@ import random
 choices = [Image.SKULL, Image.HEART]
 
 while True:
-	
-	if button_a.was_pressed():
-		
-		image = random.choice(choices)
+
+    if button_a.was_pressed():
+        image = random.choice(choices)
         display.show(image)
